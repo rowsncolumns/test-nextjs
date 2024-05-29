ba40e6e5e3ffb15d9f1e6e0e692ae077caba958d
Vinay M <rmdort@gmail.com>
2024-05-29 21:33:04 +0100

    Add spreadsheet

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack (config) {
+    config.externals.push('canvas')
+    return config
+  }
 };
 
 export default nextConfig;
